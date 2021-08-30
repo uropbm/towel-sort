678ba2f4c6c7207c9619f9c97876741aa634efa4
uropbm <spider.psu@mail.ru>
2021-08-30 20:05:19 +0300

    feat : complite task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  let arr1 = []
+  let arr2 = []
+  let arrResult = []
+
+  if (!matrix) {
+    return []
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 == 0) {
+        arr1 = matrix[i].slice(0, matrix[i].length)
+        console.log(arr1)
+        arrResult = arrResult.concat(arr1)
+    } else {
+        arr2 = matrix[i].sort((a, b) => b - a)
+        console.log(arr2)
+        arrResult = arrResult.concat(arr2)
+    }
+  }
+
+  return arrResult
+  
 }
